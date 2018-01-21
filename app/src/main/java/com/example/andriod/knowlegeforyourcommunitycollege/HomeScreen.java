ee558ca9b82bf431d1b47eb437edcd84f74b126f
Nora Dixon <noradixon03@gmail.com>
2018-01-21 17:20:41 -0600

    Initial commit

---
+++ app/src/main/java/com/example/andriod/knowlegeforyourcommunitycollege/HomeScreen.java
@@ -0,0 +1,13 @@
+package com.example.andriod.knowlegeforyourcommunitycollege;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeScreen extends AppCompatActivity {
+
+    int Yay;
+    int WoopWoop;
+
+    //Yay we have an app
+
+}
